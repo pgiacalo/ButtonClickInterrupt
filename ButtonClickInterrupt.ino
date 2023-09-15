e5adb3bcadb7db47f9fa493603675462ec625e75
Philip Giacalone <philip_giacalone@yahoo.com>
2023-09-14 21:01:27 -0700

    Added circular counter limit and checks.. Reorganized functions.

--- ButtonClickInterrupt.ino
+++ ButtonClickInterrupt.ino
@@ -1,11 +1,12 @@
 #include <Arduino.h>
 
-#define BUTTON_UP_PIN     12    // GPIO pin (note GPIO pins 0 - 33 have pullup resistors)
-#define BUTTON_DOWN_PIN   14    // GPIO pin (note GPIO pins 0 - 33 have pullup resistors)
+#define BUTTON_UP_PIN     12          // GPIO pin (note that GPIO pins 0 thru 33 have pullup resistors)
+#define BUTTON_DOWN_PIN   14          // GPIO pin (note that GPIO pins 0 thru 33 have pullup resistors)
 
-volatile bool upPressed = false;    // Flag for button press (volatile for changes by interrupt)
-volatile bool downPressed = false;  // Flag for button press (volatile for changes by interrupt)
-int counter = 0;                    // Global counter for button presses
+#define DEBUG             true        // Controls serial output messages 
+
+volatile bool upPressed = false;      // Flag for button press (volatile for changes by interrupt)
+volatile bool downPressed = false;    // Flag for button press (volatile for changes by interrupt)
 
 volatile unsigned long upInterruptTime = 0;
 volatile unsigned long upLastInterruptTime = 0;
@@ -13,8 +14,22 @@ volatile unsigned long upLastInterruptTime = 0;
 volatile unsigned long downInterruptTime = 0;
 volatile unsigned long downLastInterruptTime = 0;
 
+int counter = 0;                      // Global counter for button presses
+const int minCount = 1;               // minimum limit for circular counting
+const int maxCount = 10;              // maximum limit for circular counting
+
 void setup() {
   Serial.begin(115200);
+  initializeButtons();
+}
+
+void loop() {
+  checkForButtonPress();
+
+  //do other stuff here
+}
+
+void initializeButtons(){
   upLastInterruptTime = millis();
   upInterruptTime = upLastInterruptTime;
 
@@ -26,7 +41,6 @@ void setup() {
 
   pinMode(BUTTON_DOWN_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(BUTTON_DOWN_PIN), buttonDownISR, FALLING);
-
 }
 
 void checkForButtonPress(){
@@ -35,7 +49,13 @@ void checkForButtonPress(){
     if (upInterruptTime - upLastInterruptTime > 10){  //10 milliseconds for button bounce
       if (digitalRead(BUTTON_UP_PIN) == LOW) {
         counter++;  // Increment the counter
-        Serial.println(counter);
+        //enforce circular counting
+        if (counter > maxCount){
+          counter = minCount;
+        }
+        if (DEBUG){
+          Serial.println(counter);
+        }
         upLastInterruptTime = upInterruptTime;
       }
       
@@ -51,7 +71,13 @@ void checkForButtonPress(){
     if (downInterruptTime - downLastInterruptTime > 10){  //10 milliseconds for button bounce
       if (digitalRead(BUTTON_DOWN_PIN) == LOW) {
         counter--;  // Decrement the counter
-        Serial.println(counter);
+        //enforce circular counting
+        if (counter < minCount){
+          counter = maxCount;
+        }
+        if (DEBUG){
+          Serial.println(counter);
+        }
         downLastInterruptTime = downInterruptTime;
       }
       
@@ -63,18 +89,14 @@ void checkForButtonPress(){
   }  
 }
 
-void loop() {
-  checkForButtonPress();
-
-  //do other stuff here
-}
-
+// Interrupt Service Routine
 void buttonUpISR() {
   upPressed = true;
   upInterruptTime = millis();
   upLastInterruptTime = upInterruptTime;
 }
 
+// Interrupt Service Routine
 void buttonDownISR() {
   downPressed = true;
   downInterruptTime = millis();
