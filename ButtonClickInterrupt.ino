32b47af7166a5fe358372f834d18cf9ed474632d
Philip Giacalone <philip_giacalone@yahoo.com>
2023-09-16 00:27:55 -0700

    Added ISR explanation in comment, along with code examples

--- ButtonClickInterrupt.ino
+++ ButtonClickInterrupt.ino
@@ -1,8 +1,95 @@
-/*
- * This ESP32 code shows how to use Interrupt Service Routines (ISRs) to detect button clicks.
- * Two buttons are used to pull-down the voltage on GPIO pins 12 and 14.
- * The code is designed to safely handle switch bouncing.
- */
+/***********************************
+
+This ESP32 code shows how to use Interrupt Service Routines (ISRs) to detect button clicks.
+Two buttons are used to pull-down the voltage on GPIO pins 12 and 14.
+The ESP32 has configurable, internal PULLUP and PULLDOWN resistors that are used with the buttons.  
+Note that the internal resistors are ONLY available on GPIO pins GPIO 0 thru GPIO 33. 
+
+This code is designed to safely handle switch bouncing.
+
+NOTES ON ISRs
+-------------
+An Interrupt Service Routine (ISR) can be triggered based on different conditions of a digital signal. 
+For GPIO pins, these conditions often relate to the pin's voltage transitioning between low and high states.
+
+Using the Arduino framework for the ESP32, when you attach an interrupt to a pin using 
+  attachInterrupt()
+
+You can also specify the condition for which the ISR will be triggered:
+
+RISING: The ISR is triggered when the pin transitions from LOW to HIGH.
+  attachInterrupt(digitalPinToInterrupt(pinNumber), ISRFunction, RISING);
+
+FALLING: The ISR is triggered when the pin transitions from HIGH to LOW.
+  attachInterrupt(digitalPinToInterrupt(pinNumber), ISRFunction, FALLING);
+
+CHANGE: The ISR is triggered on any change in pin state (either from LOW to HIGH or from HIGH to LOW).
+  attachInterrupt(digitalPinToInterrupt(pinNumber), ISRFunction, CHANGE);
+
+LOW: The ISR is triggered when the pin is in a LOW state. This is level-triggered, meaning as long as the pin remains low, the interrupt remains active. 
+This mode may not be available or behave the same way on all platforms, so check the specifics for the ESP32.
+  attachInterrupt(digitalPinToInterrupt(pinNumber), ISRFunction, LOW);
+
+ISR CODE EXAMPLES
+-----------------
+Here are examples of how you can set up an ESP32 GPIO pin with an interrupt and specify the internal resistor type using the Arduino framework.
+These are just basic examples to demonstrate the usage of the internal pull-up and pull-down resistors alongside interrupts on the ESP32. 
+
+1)  Using an Internal Pull-Up Resistor with a Falling Edge Interrupt:
+    This would be appropriate for a button that connects the pin to ground when pressed:
+
+const int pinNumber = 2;  // Replace with your pin number
+
+void setup() {
+    pinMode(pinNumber, INPUT_PULLUP);  
+    attachInterrupt(digitalPinToInterrupt(pinNumber), ISRFunction, FALLING);
+}
+
+void ISRFunction() {
+    // Your interrupt code here
+}
+
+void loop() {
+    // Main loop code here
+}
+
+2)  Using an Internal Pull-Down Resistor with a Rising Edge Interrupt:
+    This could be used for a button that connects the pin to VCC (3.3V for ESP32) when pressed:
+
+const int pinNumber = 2;  // Replace with your pin number
+
+void setup() {
+    pinMode(pinNumber, INPUT_PULLDOWN);  
+    attachInterrupt(digitalPinToInterrupt(pinNumber), ISRFunction, RISING);
+}
+
+void ISRFunction() {
+    // Your interrupt code here
+}
+
+void loop() {
+    // Main loop code here
+}
+
+3)  Using an Internal Pull-Up Resistor with a Change Interrupt:
+    This will trigger the ISR on any transition, either HIGH to LOW or LOW to HIGH. Useful when you need to detect both press and release of a button:
+
+const int pinNumber = 2;  // Replace with your pin number
+
+void setup() {
+    pinMode(pinNumber, INPUT_PULLUP);  
+    attachInterrupt(digitalPinToInterrupt(pinNumber), ISRFunction, CHANGE);
+}
+
+void ISRFunction() {
+    // Your interrupt code here
+}
+
+void loop() {
+    // Main loop code here
+}
+
+***********************************/
 
 #include <Arduino.h>
 
